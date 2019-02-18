f407d8b09d904295b4d624dfccf4ef6893a423ab
Marcel van Lohuizen <mpvl@golang.org>
2019-02-18 18:30:38 +0100

    cue: hoist literal string parsing into new package
    This allows the parser and potentially other packages
    to use this code without causing cycles.
    
    Change-Id: I3df9f4a83109757f959adaecdccbc144a4975f61

--- cue/ast.go
+++ cue/ast.go
@@ -21,6 +21,7 @@ import (
 
 	"cuelang.org/go/cue/ast"
 	"cuelang.org/go/cue/build"
+	"cuelang.org/go/cue/literal"
 	"cuelang.org/go/cue/token"
 )
 
@@ -379,7 +380,7 @@ func (v *astVisitor) walk(astNode ast.Node) (value value) {
 		}
 		lit := &interpolation{baseValue: newExpr(n), k: stringKind}
 		value = lit
-		info, prefixLen, _, err := ParseQuotes(first.Value, last.Value)
+		info, prefixLen, _, err := literal.ParseQuotes(first.Value, last.Value)
 		if err != nil {
 			return v.error(n, "invalid interpolation: %v", err)
 		}
