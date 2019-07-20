8b9f8df58b435a5a42883fe7a812cabead842b92
xinau <felix.ehrenpfort@codecentric.de>
2019-07-20 11:38:27 +0000

    pkg: adding base64 encoding for strings
    cue's current implementation doesn't support encoding and decoding
    of strings with base64. a new package `encoding/base64` adds
    the missing funcionality as builtin's.
    
    Issue #61
    
    Change-Id: Ic656dee880c25f6b0a2a710e38b3ff86fb28ab8b

---
+++ pkg/encoding/base64/manual.go
@@ -0,0 +1,39 @@
+// Copyright 2018 The CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package base64
+
+import "encoding/base64"
+
+// EncodedLen returns the length in bytes of the base64 encoding
+// of an input buffer of length n.
+func EncodedLen(n int) int {
+	return base64.StdEncoding.EncodedLen(n)
+}
+
+// DecodedLen returns the maximum length in bytes of the decoded data
+// corresponding to n bytes of base64-encoded data.
+func DecodedLen(x int) int {
+	return base64.StdEncoding.DecodedLen(x)
+}
+
+// Encode returns the base64 encoding of src.
+func Encode(src []byte) string {
+	return base64.StdEncoding.EncodeToString(src)
+}
+
+// Decode returns the bytes represented by the base64 string s.
+func Decode(s string) ([]byte, error) {
+	return base64.StdEncoding.DecodeString(s)
+}
