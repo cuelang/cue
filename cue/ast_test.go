cuelang/cue

commit 3908dac7f8b41e9a8c19ee95a243684a4382ad80

cue/ast: make package clause a declaration
This makes handling comments during generation
and analysis considerably easier.

Also fixes comments printing of import specs.

Change-Id: I0a77405524b40eff47e1e1e0dd3183ed3563b263
Reviewed-on: https://cue-review.googlesource.com/c/cue/+/2865
Reviewed-by: Marcel van Lohuizen <mpvl@golang.org>

cue/ast.go
@@ -204,6 +204,9 @@ func (v *astVisitor) walk(astNode ast.Node) (ret value) {
 		}
 		ret = obj
 
+	case *ast.Package:
+		v.walk(n.Name)
+
 	case *ast.ImportDecl:
 		for _, s := range n.Specs {
 			v.walk(s)

cue/build.go
@@ -102,9 +102,7 @@ func (r *Runtime) CompileFile(file *ast.File) (*Instance, error) {
 	if err != nil {
 		return nil, err
 	}
-	if file.Name != nil {
-		p.PkgName = file.Name.Name
-	}
+	_, p.PkgName, _ = internal.PackageInfo(file)
 	return r.complete(p)
 }
 

cue/ast/ast.go
@@ -86,7 +86,10 @@ func (*ImportDecl) declNode()        {}
 func (*BadDecl) declNode()           {}
 func (*EmbedDecl) declNode()         {}
 func (*Alias) declNode()             {}
-func (*CommentGroup) declNode()      {}
+
+// Not technically declarations, but appearing at the same level.
+func (*Package) declNode()      {}
+func (*CommentGroup) declNode() {}
 
 // A Label is any prduction that can be used as a LHS label.
 type Label interface {
@@ -599,39 +602,42 @@ func (s *ImportSpec) End() token.Pos {
 	return s.Path.End()
 }
 
-// specNode() ensures that only spec nodes can be assigned to a Spec.
+// A BadDecl node is a placeholder for declarations containing
+// syntax errors for which no correct declaration nodes can be
+// created.
+type BadDecl struct {
+	comments
+	From, To token.Pos // position range of bad declaration
+}
+
+// A ImportDecl node represents a series of import declarations. A valid
+// Lparen position (Lparen.Line > 0) indicates a parenthesized declaration.
+type ImportDecl struct {
+	comments
+	Import token.Pos
+	Lparen token.Pos // position of '(', if any
+	Specs  []*ImportSpec
+	Rparen token.Pos // position of ')', if any
+}
+
+type Spec interface {
+	Node
+	specNode()
+}
+
 func (*ImportSpec) specNode() {}
 
-// A declaration is represented by one of the following declaration nodes.
-type (
-	// A BadDecl node is a placeholder for declarations containing
-	// syntax errors for which no correct declaration nodes can be
-	// created.
-	BadDecl struct {
-		comments
-		From, To token.Pos // position range of bad declaration
-	}
+func (*Package) specNode() {}
 
-	// A ImportDecl node represents a series of import declarations. A valid
-	// Lparen position (Lparen.Line > 0) indicates a parenthesized declaration.
-	ImportDecl struct {
-		comments
-		Import token.Pos
-		Lparen token.Pos // position of '(', if any
-		Specs  []*ImportSpec
-		Rparen token.Pos // position of ')', if any
-	}
-
-	// An EmbedDecl node represents a single expression used as a declaration.
-	// The expressions in this declaration is what will be emitted as
-	// configuration output.
-	//
-	// An EmbedDecl may only appear at the top level.
-	EmbedDecl struct {
-		comments
-		Expr Expr
-	}
-)
+// An EmbedDecl node represents a single expression used as a declaration.
+// The expressions in this declaration is what will be emitted as
+// configuration output.
+//
+// An EmbedDecl may only appear at the top level.
+type EmbedDecl struct {
+	comments
+	Expr Expr
+}
 
 // Pos and End implementations for declaration nodes.
 
@@ -662,19 +668,13 @@ func (d *EmbedDecl) End() token.Pos { return d.Expr.End() }
 type File struct {
 	Filename string
 	comments
-	Package token.Pos // position of "package" pseudo-keyword
-	Name    *Ident    // package names
-	Decls   []Decl    // top-level declarations; or nil
+	Decls []Decl // top-level declarations; or nil
 
-	// TODO: Change Expr to Decl?
 	Imports    []*ImportSpec // imports in this file
 	Unresolved []*Ident      // unresolved identifiers in this file
 }
 
 func (f *File) Pos() token.Pos {
-	if f.Package != token.NoPos {
-		return f.Package
-	}
 	if len(f.Decls) > 0 {
 		return f.Decls[0].Pos()
 	}
@@ -689,8 +689,29 @@ func (f *File) End() token.Pos {
 	if n := len(f.Decls); n > 0 {
 		return f.Decls[n-1].End()
 	}
-	if f.Name != nil {
-		return f.Name.End()
+	return token.NoPos
+}
+
+// A Package represents a package clause.
+type Package struct {
+	comments
+	PackagePos token.Pos // position of "package" pseudo-keyword
+	Name       *Ident    // package name
+}
+
+func (p *Package) Pos() token.Pos {
+	if p.PackagePos != token.NoPos {
+		return p.PackagePos
+	}
+	if p.Name != nil {
+		return p.Name.Pos()
+	}
+	return token.NoPos
+}
+
+func (p *Package) End() token.Pos {
+	if p.Name != nil {
+		return p.Name.End()
 	}
 	return token.NoPos
 }

cmd/cue/cmd/add.go
@@ -30,6 +30,7 @@ import (
 	"cuelang.org/go/cue/format"
 	"cuelang.org/go/cue/load"
 	"cuelang.org/go/cue/parser"
+	"cuelang.org/go/internal"
 	"github.com/spf13/cobra"
 )
 
@@ -252,9 +253,9 @@ func initFile(cmd *cobra.Command, file string, getBuild func(path string) *build
 		if err != nil {
 			return nil, err
 		}
-		if f.Name != nil {
-			if pkg := flagPackage.String(cmd); pkg != "" && f.Name.Name != pkg {
-				return nil, fmt.Errorf("package mismatch (%s vs %s) for file %s", f.Name.Name, pkg, file)
+		if _, pkgName, _ := internal.PackageInfo(f); pkgName != "" {
+			if pkg := flagPackage.String(cmd); pkg != "" && pkgName != pkg {
+				return nil, fmt.Errorf("package mismatch (%s vs %s) for file %s", pkgName, pkg, file)
 			}
 			todo.build = getBuild(dir)
 		} else {

cmd/cue/cmd/get_go.go
@@ -33,6 +33,7 @@ import (
 
 	"cuelang.org/go/cue/format"
 	"cuelang.org/go/cue/parser"
+	"cuelang.org/go/internal"
 	"github.com/spf13/cobra"
 	"golang.org/x/tools/go/packages"
 )
@@ -497,7 +498,7 @@ func (e *extractor) extractPkg(root string, p *packages.Package) error {
 				return err
 			}
 
-			if f.Name != nil && f.Name.Name == p.Name {
+			if _, pkg, _ := internal.PackageInfo(f); pkg != "" && pkg == p.Name {
 				file := filepath.Base(path)
 				file = file[:len(file)-len(".cue")]
 				file += "_gen.cue"

cmd/cue/cmd/import.go
@@ -403,9 +403,6 @@ func combineExpressions(cmd *cobra.Command, pkg, cueFile string, objs ...ast.Exp
 	}
 
 	f := &ast.File{}
-	if pkg != "" {
-		f.Name = ast.NewIdent(pkg)
-	}
 
 	h := hoister{
 		fields:   map[string]bool{},
@@ -502,6 +499,11 @@ func combineExpressions(cmd *cobra.Command, pkg, cueFile string, objs ...ast.Exp
 			}
 		}
 		f.Decls = append([]ast.Decl{imports}, f.Decls...)
+	}
+
+	if pkg != "" {
+		p := &ast.Package{Name: ast.NewIdent(pkg)}
+		f.Decls = append([]ast.Decl{p}, f.Decls...)
 	}
 
 	if flagList.Bool(cmd) {

cue/ast/walk.go
@@ -177,10 +177,10 @@ func walk(v visitor, node Node) {
 
 	// Files and packages
 	case *File:
-		if n.Name != nil {
-			walk(v, n.Name)
-		}
 		walkDeclList(v, n.Decls)
+
+	case *Package:
+		walk(v, n.Name)
 
 	case *ListComprehension:
 		walk(v, n.Expr)

cue/build/instance.go
@@ -24,6 +24,7 @@ import (
 	"cuelang.org/go/cue/errors"
 	"cuelang.org/go/cue/parser"
 	"cuelang.org/go/cue/token"
+	"cuelang.org/go/internal"
 )
 
 // An Instance describes the collection of files, and its imports, necessary
@@ -177,12 +178,7 @@ func (inst *Instance) AddFile(filename string, src interface{}) error {
 // AddSyntax adds the given file to list of files for this instance. The package
 // name of the file must match the package name of the instance.
 func (inst *Instance) AddSyntax(file *ast.File) errors.Error {
-	pkg := ""
-	pos := file.Pos()
-	if file.Name != nil {
-		pkg = file.Name.Name
-		pos = file.Name.Pos()
-	}
+	_, pkg, pos := internal.PackageInfo(file)
 	if !inst.setPkg(pkg) && pkg != inst.PkgName {
 		err := errors.Newf(pos,
 			"package name %q conflicts with previous package name %q",

commit 2ede3ef2069aa3c3261ba04376cf62aab652486e

cue: fix crash related to package name
commit 3908dac7f8b inadvertently
tried to resolve the package name
as an identifier.

This resulted in a crash if there was an
imported package with the same name.

Also catch duplicate package declarations
as an error, which previous went unreported.

Change-Id: I4fcd5f2144d434f4fa4850d4987b75f7d85f13ad

cue/ast.go
@@ -205,7 +205,8 @@ func (v *astVisitor) walk(astNode ast.Node) (ret value) {
 		ret = obj
 
 	case *ast.Package:
-		v.walk(n.Name)
+		// NOTE: Do NOT walk the identifier of the package here, as it is not
+		// supposed to resolve to anything.
 
 	case *ast.ImportDecl:
 		for _, s := range n.Specs {

cue/build.go
@@ -391,6 +391,7 @@ func resolveFile(idx *index, f *ast.File, p *build.Instance, allFields map[strin
 				"%s redeclared as imported package name\n"+
 					"\tprevious declaration at %v", name, lineStr(idx, n))
 		}
+		fields[name] = spec
 		used := false
 		for _, u := range index[name] {
 			used = true

cue/ast_test.go
@@ -16,6 +16,7 @@ package cue
 
 import (
 	"bytes"
+	"strings"
 	"testing"
 
 	"cuelang.org/go/cue/errors"
@@ -377,6 +378,57 @@ func TestEval(t *testing.T) {
 			v := testResolve(ctx, evaluated, evalFull)
 			if got := debugStr(ctx, v); got != tc.out {
 				t.Errorf("output differs:\ngot  %q\nwant %q", got, tc.out)
+			}
+		})
+	}
+}
+
+func TestResolution(t *testing.T) {
+	testCases := []struct {
+		name string
+		in   string
+		err  string
+	}{{
+		name: "package name identifier should not resolve to anything",
+		in: `package time
+
+		import "time"
+
+		a: time.Time
+		`,
+	}, {
+		name: "duplicate_imports.cue",
+		in: `
+		import "time"
+		import time "math"
+
+		t: time.Time
+		`,
+		err: "time redeclared as imported package name",
+	}, {
+		name: "unused_import",
+		in: `
+			import "time"
+			`,
+		err: `imported and not used: "time"`,
+	}, {
+		name: "nonexisting import package",
+		in:   `import "doesnotexist"`,
+		err:  `package "doesnotexist" not found`,
+	}}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var r Runtime
+			_, err := r.Compile(tc.name, tc.in)
+			got := err == nil
+			want := tc.err == ""
+			if got != want {
+				t.Fatalf("got %v; want %v", err, tc.err)
+			}
+			if err != nil {
+				if s := err.Error(); !strings.Contains(s, tc.err) {
+					t.Errorf("got %v; want %v", err, tc.err)
+				}
 			}
 		})
 	}