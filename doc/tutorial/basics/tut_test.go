dba1c3f13f9af3c94694acfd6a015ee0cd3b6945
Marcel van Lohuizen <mpvl@golang.org>
2019-09-12 16:19:27 +0200

    doc/tutorial/basics: update test to use data
    Change-Id: I057d7325618561060440c96a61351ef7f0c83c1f

--- doc/tutorial/basics/tut_test.go
+++ doc/tutorial/basics/tut_test.go
@@ -16,7 +16,7 @@ package basics
 
 import (
 	"bytes"
-	"fmt"
+	"flag"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -24,15 +24,21 @@ import (
 	"testing"
 
 	"cuelang.org/go/internal/cuetest"
+	"github.com/kylelemons/godebug/diff"
 )
+
+var update = flag.Bool("update", false, "update test data")
 
 func TestTutorial(t *testing.T) {
 	// t.Skip()
 
-	err := filepath.Walk(".", func(path string, f os.FileInfo, err error) error {
-		if strings.HasSuffix(path, ".md") && !strings.Contains(path, "/") {
-			t.Run(path, func(t *testing.T) { simulateFile(t, path) })
+	err := filepath.Walk("data", func(path string, f os.FileInfo, err error) error {
+		if !f.IsDir() || path == "data" {
+			return nil
 		}
+		t.Run(path, func(t *testing.T) {
+			simulateFile(t, path)
+		})
 		return nil
 	})
 	if err != nil {
@@ -41,122 +47,30 @@ func TestTutorial(t *testing.T) {
 }
 
 func simulateFile(t *testing.T, path string) {
-	b, err := ioutil.ReadFile(path)
+	file := filepath.Join(path, "cmd.sh")
+	b, err := ioutil.ReadFile(file)
+	if err != nil {
+		t.Skipf("failed to open file %q: %v", path, err)
+	}
+	command := string(bytes.TrimSpace(b))
+
+	out := &bytes.Buffer{}
+	cuetest.Run(t, path, command, &cuetest.Config{Stdout: out})
+
+	file = filepath.Join(path, "out.txt")
+	if *update {
+		_ = ioutil.WriteFile(file, out.Bytes(), 0644)
+		return
+	}
+
+	b, err = ioutil.ReadFile(file)
 	if err != nil {
 		t.Fatalf("failed to open file %q: %v", path, err)
 	}
+	want := strings.TrimSpace(string(b))
+	got := strings.TrimSpace(out.String())
 
-	if path == "Readme.md" {
-		return
-	}
-	body := ""
-	frontMatter := &bytes.Buffer{}
-	fmt.Fprintln(frontMatter, "+++")
-	var (
-		baseName = path[:len(path)-len(".md")]
-		docsDir  string
-	)
-
-	defer func() {
-		if len(body) == 0 {
-			return
-		}
-		fmt.Fprintln(frontMatter, "+++")
-		frontMatter.WriteString(body)
-
-		err = ioutil.WriteFile(
-			filepath.Join(docsDir, filepath.Base(path)),
-			frontMatter.Bytes(), 0644)
-
-	}()
-
-	dir, err := ioutil.TempDir("", "tutbasics")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.Remove(dir)
-
-	c := cuetest.NewChunker(t, b)
-
-	c.Find("\n")
-	c.Next("_", "_")
-	section := c.Text()
-	sub := strings.ToLower(strings.Fields(section)[0])
-	sub = strings.TrimRight(sub, ",")
-	dataDir := filepath.Join("data", baseName)
-	_ = os.MkdirAll(dataDir, 0755)
-	docsDir = filepath.Join("docs", sub)
-	_ = os.MkdirAll(docsDir, 0755)
-
-	ioutil.WriteFile(filepath.Join(docsDir, "_index.md"), []byte(
-		fmt.Sprintf(`+++
-title = %q
-weight = 2000
-description = ""
-+++`, section),
-	), 0644)
-
-	c.Next("# ", "\n")
-	fmt.Fprintf(frontMatter, "title = %q\n", c.Text())
-	fmt.Fprintf(frontMatter, "weight = 2000\n") // TODO adjust manually.
-
-	inputs := []string{}
-	// collect files
-	for i := 0; c.Find("<!-- CUE editor -->"); i++ {
-		if i == 0 {
-			body = c.Text()
-		}
-		if !c.Next("_", "_") {
-			continue
-		}
-		filename := strings.TrimRight(c.Text(), ":")
-		inputs = append(inputs, filename)
-
-		if !c.Next("```", "```") {
-			t.Fatalf("No body for filename %q in file %q", filename, path)
-		}
-		b := bytes.TrimSpace(c.Bytes())
-
-		ioutil.WriteFile(filepath.Join(dir, filename), b, 0644)
-		err := ioutil.WriteFile(filepath.Join(dataDir, filename), b, 0644)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-	key := filepath.Base(baseName)
-
-	if len(inputs) != 1 || inputs[0] != key+".cue" {
-		a := strings.Replace(fmt.Sprintf("%q", inputs), " ", ",", -1)
-		fmt.Fprintf(frontMatter, "inputs = %s\n", a)
-	}
-
-	if !c.Find("<!-- result -->") {
-		return
-	}
-
-	if !c.Next("`$ ", "`") {
-		t.Fatalf("No command for result section in file %q", path)
-	}
-	command := c.Text()
-	fmt.Fprintf(frontMatter, "exec = %q\n", command)
-	err = ioutil.WriteFile(filepath.Join(dataDir, "cmd.sh"), []byte(command), 0755)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if !c.Next("```", "```") {
-		return
-	}
-	gold := c.Text()
-	if p := strings.Index(gold, "\n"); p > 0 {
-		gold = gold[p+1:]
-	}
-
-	cuetest.Run(t, dir, command, &cuetest.Config{Golden: gold})
-
-	gold = strings.TrimSpace(gold) + "\n"
-	err = ioutil.WriteFile(filepath.Join(dataDir, "out.txt"), []byte(gold), 0644)
-	if err != nil {
-		t.Fatal(err)
+	if got != want {
+		t.Errorf("files differ:\n%s", diff.Diff(got, want))
 	}
 }
