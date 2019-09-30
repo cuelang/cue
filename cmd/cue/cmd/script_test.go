3e456097f1f8e11e405ab43857431e9fd703caec
Roger Peppe <rogpeppe@gmail.com>
2019-09-30 09:54:24 +0100

    cmd/cue/cmd: use testscript for top level command testing
    Change-Id: I023c1dc836a0bad42eb2103294ecffb58dfff8e3

---
+++ cmd/cue/cmd/script_test.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/rogpeppe/testscript"
+)
+
+func TestScript(t *testing.T) {
+	testscript.Run(t, testscript.Params{
+		Dir: "testdata/script",
+	})
+}
+
+func TestMain(m *testing.M) {
+	os.Exit(testscript.RunMain(m, map[string]func() int{
+		"cue": main,
+	}))
+}
+
+func main() int {
+	err := Main(context.Background(), os.Args[1:])
+	if err != nil {
+		if err != ErrPrintedError {
+			fmt.Println(err)
+		}
+		return 1
+	}
+	return 0
+}
