164f2fb9a80096dce7135cd7ee19dadffd986a44
Marcel van Lohuizen <mpvl@golang.org>
2020-02-24 17:10:35 +0100

    cmd/cue/cmd: streaming instance iteration
    This provides an abstraction for instance generation
    allowing more consistent behavior between modes.
    
    Steaming iteration also is necessary to incrementally
    process a large number of data files.
    
    This allows data files to be used in eval and export.
    
    Change-Id: I938f9b4380bd00af3f2671249f9e8ef3945c76cc

--- cmd/cue/cmd/eval.go
+++ cmd/cue/cmd/eval.go
@@ -97,12 +97,17 @@ func runEval(cmd *Command, args []string) error {
 		}
 	}
 
-	instances := b.instances()
-	for _, inst := range instances {
+	iter := b.instances()
+	defer iter.close()
+	for iter.scan() {
+		inst := iter.value()
+
 		// TODO: use ImportPath or some other sanitized path.
-		if len(instances) > 1 {
-			fmt.Fprintf(w, "\n// %s\n", inst.Dir)
+		if len(b.insts) > 1 {
+			fmt.Fprintf(w, "\n// %s\n", iter.id())
 		}
+		v := iter.value()
+
 		syn := []cue.Option{
 			cue.Final(), // for backwards compatibility
 			cue.Definitions(true),
@@ -124,10 +129,10 @@ func runEval(cmd *Command, args []string) error {
 		}
 
 		if b.expressions == nil {
-			v := inst.Value()
+			v := v
 			if flagConcrete.Bool(cmd) && !flagIgnore.Bool(cmd) {
 				if err := v.Validate(cue.Concrete(true)); err != nil {
-					exitIfErr(cmd, inst, err, false)
+					exitOnErr(cmd, err, false)
 					continue
 				}
 			}
@@ -138,19 +143,20 @@ func runEval(cmd *Command, args []string) error {
 				fmt.Fprint(w, "// ")
 				writeNode(format.Node(e))
 			}
-			v := inst.Eval(e)
+			v := inst.EvalExpr(e)
 			if err := v.Err(); err != nil {
 				return err
 			}
 			if flagConcrete.Bool(cmd) && !flagIgnore.Bool(cmd) {
 				if err := v.Validate(cue.Concrete(true)); err != nil {
-					exitIfErr(cmd, inst, err, false)
+					exitOnErr(cmd, err, false)
 					continue
 				}
 			}
 			writeNode(format.Node(getSyntax(v, syn), opts...))
 		}
 	}
+	exitOnErr(cmd, iter.err(), true)
 	return nil
 }
 
