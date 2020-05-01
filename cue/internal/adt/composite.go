1ea65a47973a4b840b60e370be44bd7406e632aa
Marcel van Lohuizen <mpvl@golang.org>
2020-05-01 16:00:06 +0200

    cue/internal/compile: add new compiler and adt
    adt: defined types for the new evaluator. Does not yet define behavior.
    
    compile: compiler, but still needs support for builtins and imports
    
    debug: debug representation of internal structures
    
    runtime: just string uniquing functionality for now
    
    cuetxtar: added txtar support, including auto --update and --todo
    support.
    
    Change-Id: Idadf6045867b55381f0b3e0da3bc772fa40f7a04

---
+++ cue/internal/adt/composite.go
@@ -0,0 +1,146 @@
+// Copyright 2020 CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package adt
+
+import (
+	"cuelang.org/go/cue/ast"
+)
+
+// An Environment links the parent scopes for identifier lookup to a composite
+// node. Each conjunct that make up node in the tree can be associated with
+// a different environment (although some conjuncts may share an Environment).
+type Environment struct {
+	Up     *Environment
+	Vertex *Vertex
+	cache  map[Expr]Value
+}
+
+// A Vertex is a node in the value tree. It may be a leaf or internal node.
+// It may have arcs to represent elements of a fully evaluated struct or list.
+//
+// For structs, it only contains definitions and concrete fields.
+// optional fields are dropped.
+//
+// It maintains source information such as a list of conjuncts that contributed
+// to the value.
+type Vertex struct {
+	Parent *Vertex // Do we need this?
+
+	// Label is the feature leading to this vertex.
+	Label Feature
+
+	// Value is the value associated with this vertex. For lists and structs
+	// this is a sentinel value indicating its kind.
+	Value Value
+
+	// The parent of nodes can be followed to determine the path within the
+	// configuration of this node.
+	// Value  Value
+	Arcs []*Vertex // arcs are sorted in display order.
+
+	// Conjuncts lists the structs that ultimately formed this Composite value.
+	// This includes all selected disjuncts. This information is used to compute
+	// the topological sort of arcs.
+	Conjuncts []Conjunct
+
+	// Structs is a slice of struct literals that contributed to this value.
+	Structs []*StructLit
+
+	// arcLookup may be provided as a map if arcs is larger than a certain size.
+	// arcLookup map[feature]int
+}
+
+func (v *Vertex) Kind() Kind {
+	return v.Value.Kind()
+}
+
+func (v *Vertex) IsList() bool {
+	_, ok := v.Value.(*ListMarker)
+	return ok
+}
+
+// lookup returns the Arc with label f if it exists or nil otherwise.
+//
+// It returns a pointer to the original arc to allow updating the cache.
+// The pointer should not be used after inserting other arcs into the structure.
+func (v *Vertex) lookup(f Feature) *Vertex {
+	// TODO: special case int for lists.
+
+	for _, a := range v.Arcs {
+		if a.Label == f {
+			return a
+		}
+	}
+	return nil
+}
+
+func (v *Vertex) GetArc(f Feature) (arc *Vertex, isNew bool) {
+	// if v != parent {
+	// 	panic("")
+	// }
+	arc = v.lookup(f)
+	if arc == nil {
+		arc = &Vertex{Parent: v, Label: f}
+		v.Arcs = append(v.Arcs, arc)
+		isNew = true
+	}
+	return arc, isNew
+}
+
+func (v *Vertex) Source() ast.Node { return nil }
+func (v *Vertex) AddConjunct(c Conjunct) *Bottom {
+	for _, x := range v.Conjuncts {
+		if x == c {
+			return nil
+		}
+	}
+	if v.Value != nil {
+		return &Bottom{} // Connot add conjunct.
+	}
+	v.Conjuncts = append(v.Conjuncts, c)
+	return nil
+}
+
+// An Conjunct is an Environment-Expr pair. The Environment is the starting point
+// for reference lookup for any reference contained in X.
+type Conjunct struct {
+	Env *Environment
+	x   Node
+}
+
+// TODO(perf): replace with composite literal if this helps performance.
+func MakeConjunct(env *Environment, x Node) Conjunct {
+	switch x.(type) {
+	case Expr, interface{ expr() Expr }:
+	default:
+		panic("invalid Node type")
+	}
+	return Conjunct{env, x}
+}
+
+func (c *Conjunct) Source() ast.Node {
+	return c.x.Source()
+}
+
+func (c *Conjunct) Expr() Expr {
+	switch x := c.x.(type) {
+	case Expr:
+		return x
+	case interface{ expr() Expr }:
+		return x.expr()
+	default:
+		panic("unreachable")
+	}
+}
---
+++ cue/internal/adt/kind.go
@@ -0,0 +1,156 @@
+// Copyright 2018 The CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package adt
+
+import (
+	"fmt"
+)
+
+// Concreteness is a measure of the level of concreteness of a value, where
+// lower values mean more concrete.
+type Concreteness int
+
+const (
+	BottomLevel Concreteness = iota
+
+	// Concrete indicates a concrete scalar value, list or struct.
+	Concrete
+
+	// Constraint indicates a non-concrete scalar value that is more specific,
+	// than a top-level type.
+	Constraint
+
+	// PrimitiveType indicates a top-level specific type, for instance, string,
+	// bytes, number, or bool.
+	Type
+
+	// Any indicates any value, or top.
+	Any
+)
+
+// IsConcrete returns whether a value is concrete.
+func IsConcrete(v Value) bool {
+	if x, ok := v.(*Vertex); ok {
+		v = x.Value
+	}
+	if v == nil {
+		return false
+	}
+	return v.Concreteness() <= Concrete
+}
+
+// Kind reports the Value kind.
+type Kind uint16
+
+const (
+	NullKind Kind = (1 << iota)
+	BoolKind
+	IntKind
+	FloatKind
+	StringKind
+	BytesKind
+	ListKind
+	StructKind
+
+	allKinds
+
+	BottomKind = 0
+
+	NumKind     = IntKind | FloatKind
+	TopKind     = (allKinds - 1) // all kinds, but not references
+	ScalarKinds = NullKind | BoolKind |
+		IntKind | FloatKind | StringKind | BytesKind
+
+	typeKinds = (allKinds - 1)
+
+	atomKind    = (ListKind - 1)
+	addableKind = (StructKind - 1)
+
+	comparableKind = (ListKind - 1)
+	stringableKind = ScalarKinds | StringKind
+)
+
+func isTop(v Value) bool {
+	_, ok := v.(*Top)
+	return ok
+}
+
+func isCustom(v Value) bool {
+	_, ok := v.(*BuiltinValidator)
+	return ok
+}
+
+// IsAnyOf reports whether k is any of the given kinds.
+//
+// For instances, k.IsAnyOf(String|Bytes) reports whether k overlaps with
+// the String or Bytes kind.
+func (k Kind) IsAnyOf(of Kind) bool {
+	return k&of != BottomKind
+}
+
+// CanString reports whether the given type can convert to a string.
+func (k Kind) CanString() bool {
+	return k&StringKind|ScalarKinds != BottomKind
+}
+
+// String reports the string representation of k.
+func (k Kind) String() string {
+	str := ""
+	if k&TopKind == TopKind {
+		str = "_"
+		goto finalize
+	}
+	for i := Kind(1); i < allKinds; i <<= 1 {
+		t := ""
+		switch k & i {
+		case BottomKind:
+			continue
+		case NullKind:
+			t = "null"
+		case BoolKind:
+			t = "bool"
+		case IntKind:
+			if k&FloatKind != 0 {
+				t = "number"
+			} else {
+				t = "int"
+			}
+		case FloatKind:
+			if k&IntKind != 0 {
+				continue
+			}
+			t = "float"
+		case StringKind:
+			t = "string"
+		case BytesKind:
+			t = "bytes"
+		case ListKind:
+			t = "list"
+		case StructKind:
+			t = "struct"
+		default:
+			t = fmt.Sprintf("<unknown> %x", int(i))
+		}
+		if str != "" {
+			str += "|"
+		}
+		str += t
+	}
+finalize:
+	if str == "" {
+		return "_|_"
+	}
+	return str
+}
