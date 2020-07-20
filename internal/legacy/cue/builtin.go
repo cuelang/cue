0c0cb27c364c7f68c6434bc792979410f8f2683c
Marcel van Lohuizen <mpvl@golang.org>
2020-07-20 10:36:31 +0200

    internal/core/adt: add path location for majority of errors
    Change-Id: I8a7b19851af88ecd09ae9032d659ade3570b73e3

--- internal/legacy/cue/builtin.go
+++ internal/legacy/cue/builtin.go
@@ -211,8 +211,7 @@ var lenBuiltin = &builtin{
 			}
 			c.ret = len(s)
 		default:
-			c.ret = errors.Newf(token.NoPos,
-				"invalid argument type %v", k)
+			c.ret = c.ctx.opCtx.Newf("invalid argument type %v", k)
 		}
 	},
 }
