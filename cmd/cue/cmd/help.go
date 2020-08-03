edabec388235bc4363068855d1cfd4ab3dc1cf24
chai2010 <chaishushan@gmail.com>
2020-08-03 10:05:52 +0800

    cmd/help: fix example in doc

--- cmd/cue/cmd/help.go
+++ cmd/cue/cmd/help.go
@@ -400,7 +400,7 @@ the second to standard output (i.e. it echos it again).
 		}
 
 		// starts after echo
-		file.Append & {
+		append: file.Append & {
 			filename: var.file
 			contents: echo.stdout
 		}
