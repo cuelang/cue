c4a7d76a5763c37b858625772bf83894fcc1a074
Tony Worm <tony@hofstadter.io>
2020-12-29 22:41:11 -0500

    cue get go: handle the presence of Go vendor dir without Cue source
    When 'cue get go' is run from a directory with a go vendor directory,
    and Cue is not within the dependencies and vendor directory,
    then Cue fails to load its interface types, ignoring a nested error
    from the golang.org/x/tools/go/packages.Load function.
    This changes checks for and returns these nested errors.
    It also embeds the interface files content so that this
    part of the code works regardless of modules and vendor dirs.
    
    Fixes #621
    
    Change-Id: I630e2cb167c05d63186aa356eaf70a58749b61c5

--- cmd/cue/cmd/get_go.go
+++ cmd/cue/cmd/get_go.go
@@ -281,13 +281,54 @@ func (e *extractor) usedPkg(pkg string) {
 	e.usedPkgs[pkg] = true
 }
 
-func initInterfaces() error {
-	cfg := &packages.Config{
-		Mode: packages.LoadAllSyntax,
-	}
-	p, err := packages.Load(cfg, "cuelang.org/go/cmd/cue/cmd/interfaces")
+const cueGoMod = `
+module cuelang.org/go
+`
+
+func initInterfaces() (err error) {
+	// tempdir needed for overlay
+	tmpDir, err := ioutil.TempDir("", "cuelang")
 	if err != nil {
 		return err
+	}
+
+	defer func() {
+		err = os.RemoveAll(tmpDir)
+	}()
+
+	// write the cuelang go.mod
+	err = ioutil.WriteFile(filepath.Join(tmpDir, "go.mod"), []byte(cueGoMod), 0644)
+	if err != nil {
+		return err
+	}
+
+	for fn, contents := range interfaceFiles {
+		dir := filepath.Join(tmpDir, "cmd/cue/cmd/interfaces")
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			return err
+		}
+
+		filename := filepath.Join(dir, fn)
+		if err = ioutil.WriteFile(filename, contents, 0666); err != nil {
+			return err
+		}
+	}
+
+	cfg := &packages.Config{
+		Mode: packages.LoadAllSyntax,
+		Dir:  filepath.Join(tmpDir, "cmd/cue/cmd/interfaces"),
+	}
+
+	p, err := packages.Load(cfg)
+	if err != nil {
+		return fmt.Errorf("error loading embedded cuelang.org/go/cmd/cue/cmd/interfaces package: %w", err)
+	}
+	if len(p[0].Errors) > 0 {
+		var buf bytes.Buffer
+		for _, e := range p[0].Errors {
+			fmt.Fprintf(&buf, "\t%v\n", e)
+		}
+		return fmt.Errorf("error loading embedded cuelang.org/go/cmd/cue/cmd/interfaces package:\n%s", buf.String())
 	}
 
 	for e, tt := range p[0].TypesInfo.Types {
@@ -342,6 +383,15 @@ func extract(cmd *Command, args []string) error {
 	pkgs, err := packages.Load(cfg, args...)
 	if err != nil {
 		return err
+	}
+	var errs []string
+	for _, P := range pkgs {
+		if len(P.Errors) > 0 {
+			errs = append(errs, fmt.Sprintf("\t%s: %v", P.PkgPath, P.Errors))
+		}
+	}
+	if len(errs) > 0 {
+		return fmt.Errorf("While loading Go packages:\n%s", strings.Join(errs, "\n"))
 	}
 
 	e := extractor{
