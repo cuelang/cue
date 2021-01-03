7093f9681a62b1792ea24491495660acd371f9ef
Marcel van Lohuizen <mpvl@golang.org>
2021-01-03 09:30:50 +0100

    internal/core/eval: fix result of closed embedding
    This brings the implementation in line with the
    spec.
    
    Includes a small change to the formulation
    in the spec to clarify things. But it doesn't
    aim to change the meaning.
    
    Change-Id: I35dadb4af53ea3f008e39dea92ccfef0fb33f375

--- internal/core/eval/eval.go
+++ internal/core/eval/eval.go
@@ -1577,13 +1577,15 @@ func (n *nodeContext) addStruct(
 		childEnv.Deref = env.Deref
 	}
 
+	s.Init()
+
+	if s.HasEmbed {
+		closeInfo = closeInfo.SpawnGroup(nil)
+	}
+
 	parent := n.node.AddStruct(s, childEnv, closeInfo)
 	closeInfo.IsClosed = false
 	parent.Disable = true // disable until processing is done.
-
-	hasEmbed := false
-
-	s.Init()
 
 	for _, d := range s.Decls {
 		switch x := d.(type) {
@@ -1610,8 +1612,6 @@ func (n *nodeContext) addStruct(
 			n.ifClauses = append(n.ifClauses, envYield{childEnv, x, closeInfo, nil})
 
 		case adt.Expr:
-			hasEmbed = true
-
 			// add embedding to optional
 
 			// TODO(perf): only do this if addExprConjunct below will result in
@@ -1634,7 +1634,7 @@ func (n *nodeContext) addStruct(
 		}
 	}
 
-	if !hasEmbed {
+	if !s.HasEmbed {
 		n.aStruct = s
 		n.aStructID = closeInfo
 	}
