cuelang/cue

commit 0ffde15ba7658f38ea88cf97512fc756cb6f3454

pkg/internal: fix error codes for decimal and string lists
Fixes #585

The returned errors for decimal and string lists did not
set the correct error type, causing the errors to be
treated as fatal instead of incomplete.

Not the pretiest solution. But this code is still a leftover
from the old API. Error handling should be cleaned up
at some point. It can be greatly simplified.

Change-Id: I9f41031d055351c3f46ac1fc0e567258ec0affa9
Reviewed-on: https://cue-review.googlesource.com/c/cue/+/8208
Reviewed-by: CUE cueckoo <cueckoo@gmail.com>
Reviewed-by: Marcel van Lohuizen <mpvl@golang.org>

pkg/internal/context.go
@@ -19,7 +19,6 @@ import (
 	"math/big"
 
 	"cuelang.org/go/cue"
-	"cuelang.org/go/cue/errors"
 	"cuelang.org/go/cue/token"
 	"cuelang.org/go/internal/core/adt"
 	"github.com/cockroachdb/apd/v2"
@@ -237,7 +236,21 @@ func (c *CallCtxt) DecimalList(i int) (a []*apd.Decimal) {
 		return nil
 	}
 	for j := 0; v.Next(); j++ {
-		num, err := v.Value().Decimal()
+		w := v.Value()
+		if k := w.IncompleteKind(); k&adt.NumKind == 0 {
+			err := c.ctx.NewErrf(
+				"invalid type element %d (%s) of number list argument %d", j, k, i)
+			c.Err = &callError{err}
+			break
+		}
+		if !w.IsConcrete() {
+			err := c.ctx.NewErrf(
+				"non-concrete number value for element %d of number list argument %d", j, i)
+			err.Code = adt.IncompleteError
+			c.Err = &callError{err}
+			break
+		}
+		num, err := w.Decimal()
 		if err != nil {
 			c.errf(c.src, err, "invalid list element %d in argument %d to %s: %v",
 				j, i, c.Name(), err)
@@ -257,10 +270,24 @@ func (c *CallCtxt) StringList(i int) (a []string) {
 		return nil
 	}
 	for j := 0; v.Next(); j++ {
-		str, err := v.Value().String()
+		w := v.Value()
+		if k := w.IncompleteKind(); k&adt.StringKind == 0 {
+			err := c.ctx.NewErrf(
+				"invalid type element %d (%s) of string list argument %d", j, k, i)
+			c.Err = &callError{err}
+			break
+		}
+		if !w.IsConcrete() {
+			err := c.ctx.NewErrf(
+				"non-concrete string value for element %d of string list argument %d", j, i)
+			err.Code = adt.IncompleteError
+			c.Err = &callError{err}
+			break
+		}
+		str, err := w.String()
 		if err != nil {
-			c.Err = errors.Wrapf(err, c.Pos(),
-				"element %d of list argument %d", j, i)
+			// TODO: expose wrapping
+			c.Err = &callError{c.ctx.NewErrf("element %d of list argument %d: %v", j, i, err)}
 			break
 		}
 		a = append(a, str)

cue/testdata/resolve/048_builtins.txtar
@@ -3,6 +3,11 @@
 #name: builtins
 #evalPartial
 -- in.cue --
+import (
+  "list"
+  "strings"
+)
+
 a1: {
 	a: and([b, c])
 	b: =~"oo"
@@ -18,6 +23,37 @@ o1: {
 }
 o2: o1 & {a: "foo"}
 o3: o1 & {a: "foo", b: "baz"}
+
+// Issue #585
+stringListErrors: {
+    a: {
+        x: [string, ...string]
+        result: strings.Join(x, " ")
+    }
+
+    b: {
+        x: [int, ...string]
+        result: strings.Join(x, " ")
+    }
+
+    c: {
+        x: []
+        result: strings.Join(x, int)
+    }
+}
+
+decimalListErrors: {
+    a: {
+        x: [string]
+        result: list.Avg(x)
+    }
+
+    b: {
+        x: [int, ...string]
+        result: list.Avg(x)
+    }
+}
+
 -- out/def --
 a1: {
 	a: =~"oo" & =~"fo"
@@ -48,19 +84,25 @@ o3: o1 & {
 Errors:
 o3.a: 2 errors in empty disjunction:
 o3.a: conflicting values "bar" and "foo":
-    ./in.cue:10:12
-    ./in.cue:12:5
-    ./in.cue:15:5
-    ./in.cue:15:14
+    ./in.cue:15:12
+    ./in.cue:17:5
+    ./in.cue:20:5
+    ./in.cue:20:14
 o3.a: conflicting values "baz" and "foo":
-    ./in.cue:10:9
-    ./in.cue:15:5
-    ./in.cue:15:14
-    ./in.cue:15:24
+    ./in.cue:15:9
+    ./in.cue:20:5
+    ./in.cue:20:14
+    ./in.cue:20:24
 a3.a: invalid value "bar" (out of bound =~"oo"):
-    ./in.cue:3:5
+    ./in.cue:8:5
 a3.a: invalid value "bar" (out of bound =~"fo"):
-    ./in.cue:4:5
+    ./in.cue:9:5
+stringListErrors.b.result: invalid type element 0 (int) of string list argument 0:
+    ./in.cue:31:17
+stringListErrors.c.result: cannot use int (type int) as string in argument 2 to strings.Join:
+    ./in.cue:36:33
+decimalListErrors.a.result: invalid type element 0 (string) of number list argument 0:
+    ./in.cue:43:17
 
 Result:
 (_|_){
@@ -79,9 +121,9 @@ Result:
     // [eval]
     a: (_|_){
       // [eval] a3.a: invalid value "bar" (out of bound =~"oo"):
-      //     ./in.cue:3:5
+      //     ./in.cue:8:5
       // a3.a: invalid value "bar" (out of bound =~"fo"):
-      //     ./in.cue:4:5
+      //     ./in.cue:9:5
     }
     b: (string){ =~"oo" }
     c: (string){ =~"fo" }
@@ -101,17 +143,70 @@ Result:
     a: (_|_){
       // [eval] o3.a: 2 errors in empty disjunction:
       // o3.a: conflicting values "bar" and "foo":
-      //     ./in.cue:10:12
-      //     ./in.cue:12:5
-      //     ./in.cue:15:5
-      //     ./in.cue:15:14
+      //     ./in.cue:15:12
+      //     ./in.cue:17:5
+      //     ./in.cue:20:5
+      //     ./in.cue:20:14
       // o3.a: conflicting values "baz" and "foo":
-      //     ./in.cue:10:9
-      //     ./in.cue:15:5
-      //     ./in.cue:15:14
-      //     ./in.cue:15:24
+      //     ./in.cue:15:9
+      //     ./in.cue:20:5
+      //     ./in.cue:20:14
+      //     ./in.cue:20:24
     }
     b: (string){ "baz" }
     c: (string){ "bar" }
+  }
+  stringListErrors: (_|_){
+    // [eval]
+    a: (struct){
+      x: (list){
+        0: (string){ string }
+      }
+      result: (_|_){
+        // [incomplete] stringListErrors.a.result: non-concrete string value for element 0 of string list argument 0:
+        //     ./in.cue:26:17
+      }
+    }
+    b: (_|_){
+      // [eval]
+      x: (list){
+        0: (int){ int }
+      }
+      result: (_|_){
+        // [eval] stringListErrors.b.result: invalid type element 0 (int) of string list argument 0:
+        //     ./in.cue:31:17
+      }
+    }
+    c: (_|_){
+      // [eval]
+      x: (#list){
+      }
+      result: (_|_){
+        // [eval] stringListErrors.c.result: cannot use int (type int) as string in argument 2 to strings.Join:
+        //     ./in.cue:36:33
+      }
+    }
+  }
+  decimalListErrors: (_|_){
+    // [eval]
+    a: (_|_){
+      // [eval]
+      x: (#list){
+        0: (string){ string }
+      }
+      result: (_|_){
+        // [eval] decimalListErrors.a.result: invalid type element 0 (string) of number list argument 0:
+        //     ./in.cue:43:17
+      }
+    }
+    b: (struct){
+      x: (list){
+        0: (int){ int }
+      }
+      result: (_|_){
+        // [incomplete] decimalListErrors.b.result: non-concrete number value for element 0 of number list argument 0:
+        //     ./in.cue:48:17
+      }
+    }
   }
 }

pkg/internal/builtin.go
@@ -207,6 +207,8 @@ func processErr(call *CallCtxt, errVal interface{}, ret adt.Expr) adt.Expr {
 	ctx := call.ctx
 	switch err := errVal.(type) {
 	case nil:
+	case *adt.Bottom:
+		ret = err
 	case *callError:
 		ret = err.b
 	case *json.MarshalerError:

pkg/list/testdata/gen.txtar
@@ -75,7 +75,6 @@ error in call to list.Slice: invalid index: 3 > 1
 error in call to list.Slice: negative index
 error in call to list.Slice: slice bounds out of range
 error in call to list.Take: negative index
-0: error in call to list.SortStrings: element 0 of list argument 0: cannot use value 1 (type int) as string
 Ascending.x: error in call to list.Sort: 2 errors in empty disjunction:
 Ascending.x: error in call to list.Sort: conflicting values number and {b:2} (mismatched types number and struct)
 Ascending.x: error in call to list.Sort: conflicting values string and {b:2} (mismatched types string and struct)
@@ -92,6 +91,8 @@ t20: cannot use "foo" (type string) as list in argument 1 to list.Min:
     ./in.cue:22:15
 t23: cannot use "foo" (type string) as list in argument 1 to list.Product:
     ./in.cue:25:19
+t42: invalid type element 0 (int) of string list argument 0:
+    ./in.cue:48:6
 t45: cannot use "foo" (type string) as list in argument 1 to list.Sum:
     ./in.cue:51:15
 
@@ -146,7 +147,7 @@ t39: [{
 }]
 t40: _|_ // error in call to list.Sort: Ascending.x: 2 errors in empty disjunction: (and 11 more errors)
 t41: ["a", "b"]
-t42: _|_ // error in call to list.SortStrings: element 0 of list argument 0: 0: cannot use value 1 (type int) as string (and 1 more errors)
+t42: _|_ // t42: invalid type element 0 (int) of string list argument 0 (and 1 more errors)
 t43: 10
 t44: 0
 t45: _|_ // t45: cannot use "foo" (type string) as list in argument 1 to list.Sum

pkg/strings/testdata/gen.txtar
@@ -22,7 +22,8 @@ t16: strings.MaxRunes(3) & "hello"
 t17: strings.MinRunes(10) & "hello"
 -- out/strings --
 Errors:
-0: error in call to strings.Join: element 0 of list argument 0: cannot use value 1 (type int) as string
+t2: invalid type element 0 (int) of string list argument 0:
+    ./in.cue:4:5
 t10: invalid value "quux" (does not satisfy strings.MaxRunes(3)):
     ./in.cue:12:6
     ./in.cue:12:23
@@ -38,7 +39,7 @@ t17: invalid value "hello" (does not satisfy strings.MinRunes(10)):
 
 Result:
 t1: "Hello World!"
-t2: _|_ // error in call to strings.Join: element 0 of list argument 0: 0: cannot use value 1 (type int) as string (and 1 more errors)
+t2: _|_ // t2: invalid type element 0 (int) of string list argument 0 (and 1 more errors)
 t3: 97
 t4: 'llo'
 t5: "✓ H"

commit 5949edfe3e173780155bbb18abe4af82c8f2218c

pkg/internal: fix decimal and string list errors
Fixes regression introduced in 0ffde15.
Problem was bug that converted an incomplete error to a
permanent error.

The solution is to not use the public API, but rather
the internal one, and pass errors up more directly.

This also fixes a bug with slice, which masked
unevaluated arcs with a finalized one. This issue
did not surface before as the API would proactively
evaluate arcs.  For this reason (and because slices
are officially not supported), the lower level
implementations now defensively still proactively
evaluate, even if this should not be necessary.

Fixes #776

Change-Id: I76f9e36cf28d5bde0f0a629958c7d07b909d48f5

internal/core/adt/expr.go
@@ -951,10 +951,10 @@ func (x *SliceExpr) evaluate(c *OpContext) Value {
 				c.AddBottom(&Bottom{Src: a.Source(), Err: err})
 				return nil
 			}
-			n.Arcs = append(n.Arcs, &Vertex{
-				Label:     label,
-				Conjuncts: a.Conjuncts,
-			})
+			arc := *a
+			arc.Parent = n
+			arc.Label = label
+			n.Arcs = append(n.Arcs, &arc)
 		}
 		n.status = Finalized
 		return n

pkg/internal/context.go
@@ -229,69 +229,85 @@ func (c *CallCtxt) Iter(i int) (a cue.Iterator) {
 }
 
 func (c *CallCtxt) DecimalList(i int) (a []*apd.Decimal) {
-	arg := c.args[i]
-	x := cue.MakeValue(c.ctx, arg)
-	v, err := x.List()
-	if err != nil {
+	v, ok := c.args[i].(*adt.Vertex)
+	if !ok || !v.IsList() {
+		var err error
+		if b, ok := v.BaseValue.(*adt.Bottom); ok {
+			err = &callError{b}
+		}
 		c.invalidArgType(c.args[i], i, "list", err)
 		return nil
 	}
-	for j := 0; v.Next(); j++ {
-		w := v.Value()
-		if k := w.IncompleteKind(); k&adt.NumKind == 0 {
-			err := c.ctx.NewErrf(
-				"invalid type element %d (%s) of number list argument %d", j, k, i)
-			c.Err = &callError{err}
-			break
-		}
-		if !w.IsConcrete() {
+	v.Finalize(c.ctx)
+
+	for j, w := range v.Elems() {
+		w.Finalize(c.ctx) // defensive
+		switch x := w.Value().(type) {
+		case *adt.Num:
+			a = append(a, &x.X)
+
+		case *adt.Bottom:
+			if x.IsIncomplete() {
+				c.Err = x
+				return nil
+			}
+
+		default:
+			if k := w.Kind(); k&adt.NumKind == 0 {
+				err := c.ctx.NewErrf(
+					"invalid type element %d (%s) of number list argument %d", j, k, i)
+				c.Err = &callError{err}
+				return a
+			}
+
 			err := c.ctx.NewErrf(
 				"non-concrete number value for element %d of number list argument %d", j, i)
 			err.Code = adt.IncompleteError
 			c.Err = &callError{err}
-			break
+			return nil
 		}
-		num, err := w.Decimal()
-		if err != nil {
-			c.errf(c.src, err, "invalid list element %d in argument %d to %s: %v",
-				j, i, c.Name(), err)
-			break
-		}
-		a = append(a, num)
 	}
 	return a
 }
 
 func (c *CallCtxt) StringList(i int) (a []string) {
-	arg := c.args[i]
-	x := cue.MakeValue(c.ctx, arg)
-	v, err := x.List()
-	if err != nil {
+	v, ok := c.args[i].(*adt.Vertex)
+	if !ok || !v.IsList() {
+		var err error
+		if b, ok := v.BaseValue.(*adt.Bottom); ok {
+			err = &callError{b}
+		}
 		c.invalidArgType(c.args[i], i, "list", err)
 		return nil
 	}
-	for j := 0; v.Next(); j++ {
-		w := v.Value()
-		if k := w.IncompleteKind(); k&adt.StringKind == 0 {
-			err := c.ctx.NewErrf(
-				"invalid type element %d (%s) of string list argument %d", j, k, i)
-			c.Err = &callError{err}
-			break
-		}
-		if !w.IsConcrete() {
+	v.Finalize(c.ctx)
+
+	for j, w := range v.Elems() {
+		w.Finalize(c.ctx) // defensive
+		switch x := w.Value().(type) {
+		case *adt.String:
+			a = append(a, x.Str)
+
+		case *adt.Bottom:
+			if x.IsIncomplete() {
+				c.Err = x
+				return nil
+			}
+
+		default:
+			if k := w.Kind(); k&adt.StringKind == 0 {
+				err := c.ctx.NewErrf(
+					"invalid type element %d (%s) of string list argument %d", j, k, i)
+				c.Err = &callError{err}
+				return a
+			}
+
 			err := c.ctx.NewErrf(
 				"non-concrete string value for element %d of string list argument %d", j, i)
 			err.Code = adt.IncompleteError
 			c.Err = &callError{err}
-			break
+			return nil
 		}
-		str, err := w.String()
-		if err != nil {
-			// TODO: expose wrapping
-			c.Err = &callError{c.ctx.NewErrf("element %d of list argument %d: %v", j, i, err)}
-			break
-		}
-		a = append(a, str)
 	}
 	return a
 }

cue/testdata/builtins/incomplete.txtar
@@ -1,5 +1,8 @@
 -- in.cue --
-import "list"
+import (
+  "list"
+  "strings"
+)
 
 
 list1: {
@@ -40,36 +43,54 @@ value2: {
     len('sf' | 'dd')
 }
 
+incompleteArgDecimalList: {
+    a: #a & {param: 123}
+    #a: {
+        param: int
+        transformed: +param
+        listAggregated: list.Max([transformed])
+    }
+}
+
+incompleteArgStringList: {
+    a: #a & {param: "123"}
+    #a: {
+        param: string
+        transformed: param+""
+        listAggregated: strings.Join([transformed], "-")
+    }
+}
+
 -- out/eval --
 (struct){
   list1: (struct){
     Out1: (#list){
       0: (_|_){
         // [cycle] cycle error:
-        //     ./in.cue:13:23
+        //     ./in.cue:16:23
       }
     }
     Out2: (#list){
       0: (_|_){
         // [cycle] cycle error:
-        //     ./in.cue:13:23
+        //     ./in.cue:16:23
       }
     }
     Out3: (#list){
       0: (_|_){
         // [cycle] cycle error:
-        //     ./in.cue:13:23
+        //     ./in.cue:16:23
       }
     }
     Top: (#list){
       0: (_|_){
         // [cycle] cycle error:
-        //     ./in.cue:13:23
+        //     ./in.cue:16:23
       }
     }
     _Sub: (_|_){
       // [incomplete] list1._Sub: undefined field b:
-      //     ./in.cue:16:13
+      //     ./in.cue:19:13
     }
     a: (struct){
     }
@@ -77,23 +98,23 @@ value2: {
   list2: (struct){
     Out1: (_|_){
       // [cycle] cycle error:
-      //     ./in.cue:27:21
+      //     ./in.cue:30:21
     }
     Out2: (_|_){
       // [cycle] cycle error:
-      //     ./in.cue:27:21
+      //     ./in.cue:30:21
     }
     Out3: (_|_){
       // [cycle] cycle error:
-      //     ./in.cue:27:21
+      //     ./in.cue:30:21
     }
     _Top: (_|_){
       // [cycle] cycle error:
-      //     ./in.cue:27:21
+      //     ./in.cue:30:21
     }
     #Sub: (_|_){
       // [incomplete] list2.#Sub: undefined field b:
-      //     ./in.cue:30:13
+      //     ./in.cue:33:13
     }
     a: (struct){
     }
@@ -101,12 +122,48 @@ value2: {
   value1: (struct){
     a: (_|_){
       // [incomplete] value1.a: unresolved disjunction 'sf' | 'dd' (type bytes):
-      //     ./in.cue:35:8
+      //     ./in.cue:38:8
     }
   }
   value2: (_|_){
     // [incomplete] value2: unresolved disjunction 'sf' | 'dd' (type bytes):
-    //     ./in.cue:39:5
+    //     ./in.cue:42:5
+  }
+  incompleteArgDecimalList: (struct){
+    a: (#struct){
+      param: (int){ 123 }
+      transformed: (int){ 123 }
+      listAggregated: (int){ 123 }
+    }
+    #a: (#struct){
+      param: (int){ int }
+      transformed: (_|_){
+        // [incomplete] incompleteArgDecimalList.#a.transformed: operand param of '+' not concrete (was int):
+        //     ./in.cue:49:23
+      }
+      listAggregated: (_|_){
+        // [incomplete] 0: operand param of '+' not concrete (was int):
+        //     ./in.cue:49:23
+      }
+    }
+  }
+  incompleteArgStringList: (struct){
+    a: (#struct){
+      param: (string){ "123" }
+      transformed: (string){ "123" }
+      listAggregated: (string){ "123" }
+    }
+    #a: (#struct){
+      param: (string){ string }
+      transformed: (_|_){
+        // [incomplete] incompleteArgStringList.#a.transformed: non-concrete value string in operand to +:
+        //     ./in.cue:58:22
+      }
+      listAggregated: (_|_){
+        // [incomplete] 0: non-concrete value string in operand to +:
+        //     ./in.cue:58:22
+      }
+    }
   }
 }
 -- out/compile --
@@ -151,5 +208,29 @@ value2: {
   }
   value2: {
     len(('sf'|'dd'))
+  }
+  incompleteArgDecimalList: {
+    a: (〈0;#a〉 & {
+      param: 123
+    })
+    #a: {
+      param: int
+      transformed: +〈0;param〉
+      listAggregated: 〈import;list〉.Max([
+        〈0;transformed〉,
+      ])
+    }
+  }
+  incompleteArgStringList: {
+    a: (〈0;#a〉 & {
+      param: "123"
+    })
+    #a: {
+      param: string
+      transformed: (〈0;param〉 + "")
+      listAggregated: 〈import;strings〉.Join([
+        〈0;transformed〉,
+      ], "-")
+    }
   }
 }