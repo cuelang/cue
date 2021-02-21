24d43c435d1da50c6eaecfcb97e85cd5e24945bf
K. Alex Mills <k.alex.mills@gmail.com>
2021-02-21 10:48:23 -0600

    pkg/crypto/hmac: add crypto/hmac support
    Adds interface for the standard library crypto/hmac package, along with a
    way to select from a list of supported hash functions.
    
    Fixes #788
    
    Change-Id: Ifdbe031a6da4a8690448ae66d562079068539449

---
+++ pkg/crypto/hmac/hmac.go
@@ -0,0 +1,58 @@
+package hmac
+
+import (
+	"crypto/hmac"
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"fmt"
+	"hash"
+)
+
+const (
+	MD5        = "MD5"
+	SHA1       = "SHA1"
+	SHA224     = "SHA224"
+	SHA256     = "SHA256"
+	SHA384     = "SHA384"
+	SHA512     = "SHA512"
+	SHA512_224 = "SHA512_224"
+	SHA512_256 = "SHA512_256"
+)
+
+// Sign returns the HMAC signature of the data, using the provided key and hash function.
+//
+// Supported hash functions: "MD5", "SHA1", "SHA224", "SHA256", "SHA384", "SHA512", "SHA512_224",
+// and "SHA512_256".
+func Sign(hashName string, data []byte, key []byte) ([]byte, error) {
+	hash, err := hashFromName(hashName)
+	if err != nil {
+		return nil, err
+	}
+	mac := hmac.New(hash, key)
+	mac.Write(data)
+	return mac.Sum(nil), nil
+}
+
+func hashFromName(hash string) (func() hash.Hash, error) {
+	switch hash {
+	case MD5:
+		return md5.New, nil
+	case SHA1:
+		return sha1.New, nil
+	case SHA224:
+		return sha256.New224, nil
+	case SHA256:
+		return sha256.New, nil
+	case SHA384:
+		return sha512.New384, nil
+	case SHA512:
+		return sha512.New, nil
+	case SHA512_224:
+		return sha512.New512_224, nil
+	case SHA512_256:
+		return sha512.New512_256, nil
+	}
+	return nil, fmt.Errorf("unsupported hash function")
+}
