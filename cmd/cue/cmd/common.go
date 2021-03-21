cuelang/cue

commit 304a3e0e7aa78aed76a545dc030148f50a53e15a

cmd/cue/cmd: use unify semantics merging across packages
This includes two changes:
1) Use Unify instead of Fill to merge values
2) Change instance iteration to value iteration.

Instead of merging all files into a single file using the
semantics of embedding, we should use that of
unification if files are anonymous or originate from
different packages.

This is easier to do when all values in play are cue.Value.
The goal is to ultimately move away from cue.Instance
anwyay, so this is a first step in this simplification.

Fixes #743
Fixes #752

Change-Id: Ia0b3f367527eb97d32e665ba000d7bbbc14965ca
Reviewed-on: https://cue-review.googlesource.com/c/cue/+/8763
Reviewed-by: CUE cueckoo <cueckoo@gmail.com>
Reviewed-by: Paul Jolly <paul@myitcv.org.uk>

cmd/cue/cmd/common.go
@@ -16,7 +16,6 @@ package cmd
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"os"
 	"path/filepath"
@@ -169,7 +168,7 @@ func (b *buildPlan) instances() iterator {
 
 type iterator interface {
 	scan() bool
-	instance() *cue.Instance
+	value() cue.Value
 	file() *ast.File // may return nil
 	err() error
 	close()
@@ -187,11 +186,11 @@ func (i *instanceIterator) scan() bool {
 	return i.i < len(i.a) && i.e == nil
 }
 
-func (i *instanceIterator) close()                  {}
-func (i *instanceIterator) err() error              { return i.e }
-func (i *instanceIterator) instance() *cue.Instance { return i.a[i.i] }
-func (i *instanceIterator) file() *ast.File         { return nil }
-func (i *instanceIterator) id() string              { return i.a[i.i].Dir }
+func (i *instanceIterator) close()           {}
+func (i *instanceIterator) err() error       { return i.e }
+func (i *instanceIterator) value() cue.Value { return i.a[i.i].Value() }
+func (i *instanceIterator) file() *ast.File  { return nil }
+func (i *instanceIterator) id() string       { return i.a[i.i].Dir }
 
 type streamingIterator struct {
 	r    *cue.Runtime
@@ -201,7 +200,7 @@ type streamingIterator struct {
 	cfg  *encoding.Config
 	a    []*build.File
 	dec  *encoding.Decoder
-	i    *cue.Instance
+	v    cue.Value
 	f    *ast.File
 	e    error
 }
@@ -240,8 +239,8 @@ func newStreamingIterator(b *buildPlan) *streamingIterator {
 	return i
 }
 
-func (i *streamingIterator) file() *ast.File         { return i.f }
-func (i *streamingIterator) instance() *cue.Instance { return i.i }
+func (i *streamingIterator) file() *ast.File  { return i.f }
+func (i *streamingIterator) value() cue.Value { return i.v }
 
 func (i *streamingIterator) id() string {
 	if i.inst != nil {
@@ -284,15 +283,12 @@ func (i *streamingIterator) scan() bool {
 		i.e = err
 		return false
 	}
-	i.i = inst
+	i.v = inst.Value()
 	if i.base.Exists() {
 		i.e = i.base.Err()
 		if i.e == nil {
-			i.i, i.e = i.i.Fill(i.base)
-			i.i.DisplayName = internal.DebugStr(i.b.schema)
-			if inst.DisplayName != "" {
-				i.i.DisplayName = fmt.Sprintf("%s|%s", inst.DisplayName, i.i.DisplayName)
-			}
+			i.v = i.v.Unify(i.base)
+			i.e = i.v.Err()
 		}
 		i.f = nil
 	}
@@ -339,15 +335,11 @@ func (i *expressionIter) scan() bool {
 
 func (i *expressionIter) file() *ast.File { return nil }
 
-func (i *expressionIter) instance() *cue.Instance {
+func (i *expressionIter) value() cue.Value {
 	if len(i.expr) == 0 {
-		return i.iter.instance()
+		return i.iter.value()
 	}
-	inst := i.iter.instance()
-	v := i.iter.instance().Eval(i.expr[i.i])
-	ni := internal.MakeInstance(v).(*cue.Instance)
-	ni.DisplayName = fmt.Sprintf("%s|%s", inst.DisplayName, i.expr[i.i])
-	return ni
+	return internal.EvalExpr(i.iter.value(), i.expr[i.i]).(cue.Value)
 }
 
 type config struct {

cmd/cue/cmd/def.go
@@ -64,7 +64,7 @@ func runDef(cmd *Command, args []string) error {
 			err := e.EncodeFile(f)
 			exitOnErr(cmd, err, true)
 		} else {
-			err := e.Encode(iter.instance())
+			err := e.Encode(iter.value())
 			exitOnErr(cmd, err, true)
 		}
 	}

internal/encoding/encoder.go
@@ -40,7 +40,7 @@ import (
 type Encoder struct {
 	cfg          *Config
 	close        func() error
-	interpret    func(*cue.Instance) (*ast.File, error)
+	interpret    func(cue.Value) (*ast.File, error)
 	encFile      func(*ast.File) error
 	encValue     func(cue.Value) error
 	autoSimplify bool
@@ -79,7 +79,8 @@ func NewEncoder(f *build.File, cfg *Config) (*Encoder, error) {
 	case build.OpenAPI:
 		// TODO: get encoding options
 		cfg := &openapi.Config{}
-		e.interpret = func(i *cue.Instance) (*ast.File, error) {
+		e.interpret = func(v cue.Value) (*ast.File, error) {
+			i := internal.MakeInstance(v).(*cue.Instance)
 			return openapi.Generate(i, cfg)
 		}
 	// case build.JSONSchema:
@@ -204,26 +205,25 @@ func (e *Encoder) EncodeFile(f *ast.File) error {
 	return e.encodeFile(f, e.interpret)
 }
 
-func (e *Encoder) Encode(inst *cue.Instance) error {
+func (e *Encoder) Encode(v cue.Value) error {
 	e.autoSimplify = true
 	if e.interpret != nil {
-		f, err := e.interpret(inst)
+		f, err := e.interpret(v)
 		if err != nil {
 			return err
 		}
 		return e.encodeFile(f, nil)
 	}
-	v := inst.Value()
 	if err := v.Validate(cue.Concrete(e.concrete)); err != nil {
 		return err
 	}
 	if e.encValue != nil {
-		return e.encValue(inst.Value())
+		return e.encValue(v)
 	}
-	return e.encFile(valueToFile(inst.Value()))
+	return e.encFile(valueToFile(v))
 }
 
-func (e *Encoder) encodeFile(f *ast.File, interpret func(*cue.Instance) (*ast.File, error)) error {
+func (e *Encoder) encodeFile(f *ast.File, interpret func(cue.Value) (*ast.File, error)) error {
 	if interpret == nil && e.encFile != nil {
 		return e.encFile(f)
 	}
@@ -234,13 +234,13 @@ func (e *Encoder) encodeFile(f *ast.File, interpret func(*cue.Instance) (*ast.Fi
 		return err
 	}
 	if interpret != nil {
-		return e.Encode(inst)
+		return e.Encode(inst.Value())
 	}
 	v := inst.Value()
 	if err := v.Validate(cue.Concrete(e.concrete)); err != nil {
 		return err
 	}
-	return e.encValue(inst.Value())
+	return e.encValue(v)
 }
 
 func writer(f *build.File, cfg *Config) (_ io.Writer, close func() error, err error) {

cmd/cue/cmd/eval.go
@@ -117,7 +117,7 @@ func runEval(cmd *Command, args []string) error {
 		if len(b.insts) > 1 {
 			id = iter.id()
 		}
-		v := iter.instance().Value()
+		v := iter.value()
 
 		if flagConcrete.Bool(cmd) {
 			syn = append(syn, cue.Concrete(true))

cmd/cue/cmd/export.go
@@ -114,8 +114,8 @@ func runExport(cmd *Command, args []string) error {
 	iter := b.instances()
 	defer iter.close()
 	for iter.scan() {
-		inst := iter.instance()
-		err = enc.Encode(inst)
+		v := iter.value()
+		err = enc.Encode(v)
 		exitOnErr(cmd, err, true)
 	}
 	exitOnErr(cmd, iter.err(), true)

cmd/cue/cmd/vet.go
@@ -107,7 +107,7 @@ func doVet(cmd *Command, args []string) error {
 	iter := b.instances()
 	defer iter.close()
 	for iter.scan() {
-		inst := iter.instance()
+		v := iter.value()
 		// TODO: use ImportPath or some other sanitized path.
 
 		concrete := true
@@ -124,9 +124,9 @@ func doVet(cmd *Command, args []string) error {
 			cue.Hidden(true),
 		}
 		w := cmd.Stderr()
-		err := inst.Value().Validate(append(opt, cue.Concrete(concrete))...)
+		err := v.Validate(append(opt, cue.Concrete(concrete))...)
 		if err != nil && !hasFlag {
-			err = inst.Value().Validate(append(opt, cue.Concrete(false))...)
+			err = v.Validate(append(opt, cue.Concrete(false))...)
 			if !shown && err == nil {
 				shown = true
 				p := message.NewPrinter(getLang())
@@ -150,7 +150,7 @@ func vetFiles(cmd *Command, b *buildPlan) {
 	iter := b.instances()
 	defer iter.close()
 	for iter.scan() {
-		v := iter.instance().Value()
+		v := iter.value()
 
 		// Always concrete when checking against concrete files.
 		err := v.Validate(cue.Concrete(true))

commit 559f36d99e1d1895f10b2e2e5ee7e0d62d2cced8

encoding/openapi: fix title handling regression
This regression was a result of forgetting to unskip the
def_openapi test in the move to v0.3, which allowed the
regression to be introduced by 304a3e0e to go unnoticed.

This partially unwinds 304a3e0e: it still assumes the main
operation is based on Value, but if available, it also stores
the Instance so that its meta information can be extracted.

In principle all information available in the Instance is also
available in the auxiliary structures of the Value (conjuncts).
But for now this is an easier approach.

The tests have a few changes as a result. The updated meta
data is correct. Furthermore there are additional file comments
at the top. It is debatable whether the latter is desirable or not,
but we'll leave it for now, as it is the current behavior.

Change-Id: Iaf40a9e525ff582e24ab4bc9fe690a40fa146559

cmd/cue/cmd/common.go
@@ -169,7 +169,8 @@ func (b *buildPlan) instances() iterator {
 type iterator interface {
 	scan() bool
 	value() cue.Value
-	file() *ast.File // may return nil
+	instance() *cue.Instance // may return nil
+	file() *ast.File         // may return nil
 	err() error
 	close()
 	id() string
@@ -186,11 +187,12 @@ func (i *instanceIterator) scan() bool {
 	return i.i < len(i.a) && i.e == nil
 }
 
-func (i *instanceIterator) close()           {}
-func (i *instanceIterator) err() error       { return i.e }
-func (i *instanceIterator) value() cue.Value { return i.a[i.i].Value() }
-func (i *instanceIterator) file() *ast.File  { return nil }
-func (i *instanceIterator) id() string       { return i.a[i.i].Dir }
+func (i *instanceIterator) close()                  {}
+func (i *instanceIterator) err() error              { return i.e }
+func (i *instanceIterator) value() cue.Value        { return i.a[i.i].Value() }
+func (i *instanceIterator) instance() *cue.Instance { return i.a[i.i] }
+func (i *instanceIterator) file() *ast.File         { return nil }
+func (i *instanceIterator) id() string              { return i.a[i.i].Dir }
 
 type streamingIterator struct {
 	r    *cue.Runtime
@@ -239,8 +241,9 @@ func newStreamingIterator(b *buildPlan) *streamingIterator {
 	return i
 }
 
-func (i *streamingIterator) file() *ast.File  { return i.f }
-func (i *streamingIterator) value() cue.Value { return i.v }
+func (i *streamingIterator) file() *ast.File         { return i.f }
+func (i *streamingIterator) value() cue.Value        { return i.v }
+func (i *streamingIterator) instance() *cue.Instance { return nil }
 
 func (i *streamingIterator) id() string {
 	if i.inst != nil {
@@ -333,7 +336,8 @@ func (i *expressionIter) scan() bool {
 	return true
 }
 
-func (i *expressionIter) file() *ast.File { return nil }
+func (i *expressionIter) file() *ast.File         { return nil }
+func (i *expressionIter) instance() *cue.Instance { return nil }
 
 func (i *expressionIter) value() cue.Value {
 	if len(i.expr) == 0 {

cmd/cue/cmd/def.go
@@ -60,13 +60,15 @@ func runDef(cmd *Command, args []string) error {
 	iter := b.instances()
 	defer iter.close()
 	for i := 0; iter.scan(); i++ {
+		var err error
 		if f := iter.file(); f != nil {
-			err := e.EncodeFile(f)
-			exitOnErr(cmd, err, true)
+			err = e.EncodeFile(f)
+		} else if i := iter.instance(); i != nil {
+			err = e.EncodeInstance(iter.instance())
 		} else {
-			err := e.Encode(iter.value())
-			exitOnErr(cmd, err, true)
+			err = e.Encode(iter.value())
 		}
+		exitOnErr(cmd, err, true)
 	}
 	exitOnErr(cmd, iter.err(), true)
 	return nil

internal/encoding/encoder.go
@@ -45,6 +45,7 @@ type Encoder struct {
 	encValue     func(cue.Value) error
 	autoSimplify bool
 	concrete     bool
+	instance     *cue.Instance
 }
 
 // IsConcrete reports whether the output is required to be concrete.
@@ -80,7 +81,10 @@ func NewEncoder(f *build.File, cfg *Config) (*Encoder, error) {
 		// TODO: get encoding options
 		cfg := &openapi.Config{}
 		e.interpret = func(v cue.Value) (*ast.File, error) {
-			i := internal.MakeInstance(v).(*cue.Instance)
+			i := e.instance
+			if i == nil {
+				i = internal.MakeInstance(v).(*cue.Instance)
+			}
 			return openapi.Generate(i, cfg)
 		}
 	// case build.JSONSchema:
@@ -203,6 +207,15 @@ func NewEncoder(f *build.File, cfg *Config) (*Encoder, error) {
 func (e *Encoder) EncodeFile(f *ast.File) error {
 	e.autoSimplify = false
 	return e.encodeFile(f, e.interpret)
+}
+
+// EncodeInstance is as Encode, but stores instance information. This should
+// all be retrievable from the value itself.
+func (e *Encoder) EncodeInstance(v *cue.Instance) error {
+	e.instance = v
+	err := e.Encode(v.Value())
+	e.instance = nil
+	return err
 }
 
 func (e *Encoder) Encode(v cue.Value) error {