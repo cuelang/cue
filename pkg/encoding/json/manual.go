da6249b6eccf97a506bd75ed90e91950b5cf4090
Marcel van Lohuizen <mpvl@golang.org>
2021-04-16 08:31:32 +0200

    cue: introduce Context to replace Runtime
    Also:
    - move the API to be Value- instead of Instance-based.
    - Add Encode method
    - factor out build code to internal/core/
    
    Change-Id: Id6a71752b75422b972dca15c7ab9423401cb0287

--- pkg/encoding/json/manual.go
+++ pkg/encoding/json/manual.go
@@ -22,7 +22,7 @@ import (
 	"cuelang.org/go/cue"
 	"cuelang.org/go/cue/ast"
 	"cuelang.org/go/cue/parser"
-	"cuelang.org/go/internal"
+	"cuelang.org/go/internal/value"
 )
 
 // Compact generates the JSON-encoded src with insignificant space characters
@@ -109,7 +109,7 @@ func Validate(b []byte, v cue.Value) (bool, error) {
 	if !json.Valid(b) {
 		return false, fmt.Errorf("json: invalid JSON")
 	}
-	r := internal.GetRuntime(v).(*cue.Runtime)
+	r := value.ConvertToRuntime(v.Context())
 	inst, err := r.Compile("json.Validate", b)
 	if err != nil {
 		return false, err
